6edf47a24b99601df5bfa1239cad5e392a306bad
Jahirul <mdjahirulislam931559@gmail.com>
2025-11-10 18:42:58 +0600

    how make live server

---
+++ index01_statements.js
@@ -0,0 +1,16 @@
+console.log("hello");
+
+
+// document.getElementById('title').innerHTML = 'hello world';
+
+let sum=50+50;
+console.log(sum);
+  
+
+if(sum>100){
+
+  console.log('expensive');
+
+}else{
+  console.log('cheep');
+}
