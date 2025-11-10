f627a4457d1be63be819364cf3e858e5b8e0f44f
Jahirul <mdjahirulislam931559@gmail.com>
2025-11-11 00:22:45 +0600

    how to used data type variabls

---
+++ index06_data_types.js
@@ -0,0 +1,90 @@
+let firstName='Joy';
+let fullName="MD Jahirul Islam 'Joy' ";
+let bar="my country name'bangladesh'";
+console.log(firstName+"\n"+fullName+"\n"+bar);
+// console.log(fullName)
+
+
+
+
+/**let variable1 = "First line content";
+let variable2 = "Second line content";
+
+console.log(`${variable1}\n${variable2}`); */
+
+console.log("*******next");
+
+n1="joy ahmed";
+n2="jahirul";
+console.log(`my name is ${n1}`);
+
+let age1=25;
+let age2=26;
+
+let n3="ahmed islam";
+
+
+// console.log(age1+age2+n3);
+let calculation=age1+age2+n3+age1+age2;
+console.log(calculation);
+
+console.log(typeof calculation);
+
+
+/*let a=5525;
+let b=10;
+console.log(a*b+6500);*/
+
+console.log("*****bigIn types*******");
+
+/**If you add an (n/bigIn) after a large number,
+will you get the same number or not get the same number---(n) mean bigIn number*/
+
+
+
+let bigNumber=999999999999999999n;
+let bigNumber2=999999999999999999n;
+console.log(bigNumber+bigNumber2);
+
+
+console.log("***boolean type***");
+
+let ifadult=false;
+let ifadult2=true;
+console.log(ifadult,ifadult2);
+console.log(typeof ifadult, typeof ifadult2);
+
+
+console.log("****object types****");
+let info=null;
+console.log(info);
+console.log(typeof info);
+
+let fruits=['apple','orange','lemon','peer','mango'];
+
+
+fruits[2]="rice";
+fruits[0]="fesh";
+
+console.log(fruits);
+console.log(typeof fruits);
+
+
+const persion={
+
+Name:'joy',
+age:30,
+};
+/*jodi block er bahir kono kichu information add korthe tahole eivabe kora jabe!*/
+persion.country='bangladesh';
+persion.id ='0112320195';
+persion.dep='cse';
+persion.city='brahmanbaria';
+persion.university='united international university';
+persion.cgpa='2.27';
+persion.age='25';
+
+
+console.log(persion);
+console.log(typeof persion);
+
