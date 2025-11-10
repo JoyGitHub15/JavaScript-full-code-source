629dda0289909378054de21e7947f1ef7728d90a
Jahirul <mdjahirulislam931559@gmail.com>
2025-11-10 20:16:37 +0600

    how to make or used variables

---
+++ index04_Variables.js
@@ -0,0 +1,104 @@
+/*var variable
+
+you can re assaign
+you can re declare
+global scooped
+var is hosted
+
+*/
+
+
+
+var a=10;
+var a=20;
+a='bangladesh;'
+console.log(a)
+{
+  a=40;
+  console.log(a)
+  var block='bangladesh';
+}
+console.log(block);
+console.log(a);
+
+
+
+let fruits='orange';
+
+console.log( fruits,' it is my favourite food');
+
+console.log(country);
+
+var country='bangla';
+
+
+// var countrys;
+// console.log(countrys);
+// countrys='banglaaa';
+
+
+
+/*//let variable //
+
+you can not re declare
+you can not re assaign
+block Scooped
+let is not hosted
+
+*/
+
+
+
+// {
+// let bar=10;
+// let bar=30;
+// bar=70;  //it's can not work under the block
+// }
+let bar=10;
+// let bar=30; //it's can not work(let) same variable
+
+console.log(bar);
+
+let bol=30;
+ bol=60;
+{
+  console.log(bol)
+}
+
+
+/*//const variable //
+
+you can not re declare
+you can not re assaign
+block Scooped
+const is not hosted
+
+*/
+
+
+const c=10;
+//const c=20; 
+// c=30; 
+ {
+  const d=40;
+console.log(c);
+console.log(d);
+}
+
+
+const persion={
+
+Name:'joy',
+age:30,
+};
+/*jodi block er bahir kono kichu information add korthe tahole eivabe kora jabe!*/
+persion.country='bangladesh';
+persion.id ='0112320195';
+persion.dep='cse';
+persion.city='brahmanbaria';
+persion.university='united international university';
+persion.cgpa='2.27';
+persion.age='25';
+
+
+console.log(persion)
