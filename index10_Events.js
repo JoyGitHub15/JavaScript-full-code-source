44e2501c0ab6f8e52488729a0d5892dd2b68e049
Jahirul <mdjahirulislam931559@gmail.com>
2025-11-12 10:15:30 +0600

    how make to event and how to used

---
+++ index10_Events.js
@@ -0,0 +1,32 @@
+function subscribe(){
+
+document.getElementById('text').innerHTML='Subscribe';
+document.getElementById('btn').innerHTML='subscribe';
+document.getElementById('btn').style.bacKgroundColor='#262626';
+
+}
+
+
+
+
+// function like(){
+//   document.getElementById('like').innerHTML=' <i class="fa-solid fa-thumbs-up"></i>liked';
+  
+// }
+
+
+function keyPress(){
+  document.getElementById("text").innerHTML='key pressed'
+}
+
+// function onload(){
+//   alert('JavaScript is loading');
+//   document.getElementById('text').innerHTML='WEBSITE LOEDED PROPERLY';
+//   // alert('JavaScript loading is succesfull');
+// }
+
+function windowResize(){
+  document.getElementById('textarea').style.height='100px';
+  document.getElementById('form').style.fontFamily='Courier New';
+  document.getElementById('form').style.Color='#ff0000';
+}
