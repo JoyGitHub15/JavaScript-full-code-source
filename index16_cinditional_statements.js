638eb394940badd91024edf32461ea65e424711f
Jahirul <mdjahirulislam931559@gmail.com>
2025-11-26 09:33:28 +0600

    conditional statements

---
+++ index16_cinditional_statements.js
@@ -0,0 +1,18 @@
+// JavaScript Conditional Statements
+
+
+let adult =18;
+let age=prompt('how old are you age');
+if(adult<age){
+  console.log('you can vote');
+
+}else if(adult<=age){
+  console.log('you can vote2');
+
+} else if(olderman<=age){
+
+  console.log('you are older man so you can vote');
+}
+else{
+  console.log('you can not vote');
+}
