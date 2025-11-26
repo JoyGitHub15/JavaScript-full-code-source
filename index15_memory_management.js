dbc2dd2e879618e9afc4fff648c2f88117c8be42
Jahirul <mdjahirulislam931559@gmail.com>
2025-11-26 08:42:55 +0600

    memory management

---
+++ index15_memory_management.js
@@ -0,0 +1,29 @@
+// JavaScript memory two type 
+
+/*call satck
+!primitive data type .
+
+string , Number Boolean,Undefined, Null, Sybl, BigInt
+
+*/
+
+
+let FullName='Ali Hossain';
+
+let webdeveloper=FullName;
+ webdeveloper='joy'
+
+
+console.log(FullName);
+console.log(webdeveloper);
+
+
+
+
+
+
+/* heap
+!nonprimitive data type .
+object,Array
+
+*/
