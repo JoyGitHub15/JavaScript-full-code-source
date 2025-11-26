2b27dc056b03d3b967c7f6780a1586635a633b96
Jahirul <mdjahirulislam931559@gmail.com>
2025-11-26 21:18:45 +0600

    make loops

---
+++ index17_loops.js
@@ -0,0 +1,44 @@
+let car=['bmw' ,'sdf','sdfs','wtrt','weyu','real'];
+
+
+// for(let value=0;value<car.length;value++){
+// console.log(car[value]);
+// }
+
+
+
+//for in loop...!
+
+
+// let persion={
+//   name: 'joy',
+//   age: 25,
+//   country: 'Bangladesh',
+
+// }
+
+// console.log(persion);
+
+
+// for(i in persion){
+
+//   let capLetter=i.charAt(0).toUpperCase()+i.slice(1);
+
+//   console.log(capLetter+": "+persion[i]);
+
+// }
+
+
+//forEach Method...!
+
+// car.forEach(function(i){
+//   console.log(i);
+// });
+
+
+//for of loops
+
+for(i of car){
+  console.log(i);
+}
+
