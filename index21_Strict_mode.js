f813243b71fcf5566a0417386f7622b4e969a147
Jahirul <mdjahirulislam931559@gmail.com>
2025-11-27 12:24:40 +0600

    use strict how facilitys

---
+++ index21_Strict_mode.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let x=20;
+console.log(x);
+
+Number();
+function Number(){
+ let y=34;
+  console.log(y);
+}
+
+
+function Number2(p1,p2){
+console.log(p1,p2);
+}
+
+Number2(45,56);
